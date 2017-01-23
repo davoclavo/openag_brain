d1e12642d84b84978b2cbbba1de8ee6d7629ac96
David <david.gurquiza@gmail.com>
2017-01-22 19:46:22 -0800

    Add boolean direct open-loop controller

---
+++ src/openag_brain/software_modules/bool_direct_controller.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+Python implementation an open-loop controller that just interprets a Bool
+setpoint as a direct command to the actuator
+"""
+import rospy
+from controllers import OpenLoopController
+from std_msgs.msg import Bool
+
+class BoolDirect(OpenLoopController):
+    output_type = Bool
+    def update(self, set_point):
+        return set_point
+
+if __name__ == '__main__':
+    BoolDirect.start()
