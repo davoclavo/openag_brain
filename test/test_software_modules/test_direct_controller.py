e3df8aa30148aa87e3c392a6fa1aba4e1229cb27
David <david.gurquiza@gmail.com>
2017-01-22 21:51:34 -0800

    Remove useless attr setting

--- test/test_software_modules/test_direct_controller.py
+++ test/test_software_modules/test_direct_controller.py
@@ -3,6 +3,5 @@ from openag_brain.software_modules.direct_controller import Direct
 
 def test_direct_controller():
     direct = Direct()
-    direct.set_point = 0
     assert_equal(1, direct.update(1))
     assert_equal(-1, direct.update(-1))
